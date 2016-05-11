f13d70bc9c0e70a0c40576090583e6815eb94c33
danaja <danajamkdt@gmail.com>
2016-05-11 13:04:22 +0530

    provide search radius, lat,lon file for search locations in CMB district

--- fsqanalysis/loader.py
+++ fsqanalysis/loader.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 import foursquare
 from pymongo import MongoClient
-
 import csv
+import sys
+reload(sys)
 
-c = MongoClient() 
+
+sys.setdefaultencoding('utf-8')
+
+
+c = MongoClient()
 fsqdb = c.fsqdb
 ctgys= fsqdb.categories
 vns = fsqdb.venues
+
 
 with open('credentials.txt', 'r') as f:
        credentials = []
@@ -18,30 +24,25 @@ client = foursquare.Foursquare(client_id=values[0]
                     , client_secret=values[1])
 
 
-categoryResponse = client.venues.categories()
-
 def setup_category(category,parents,ctgys_col):
     ctgys_col .update({'category': category['name']},{"$set" :{'parents' : parents,'id' : category['id']}},upsert = True)
-    new_parents = list(parents) 
+    new_parents = list(parents)
     new_parents.append(category['name'])
     if 'categories' in category.keys():
-        for sub in category['categories']:        
-            setup_category(sub,new_parents,ctgys_col)             
-        
-
-for category in categoryResponse['categories']:
-    setup_category(category,[],ctgys)
+        for sub in category['categories']:
+            setup_category(sub,new_parents,ctgys_col)
 
 
-with open('1km_grid_centers.csv', 'rb') as csvfile:
-    locreader =csv.DictReader(csvfile)
-    for row in locreader:
-      venueResponse = client.venues.search(params={'ll': row['latitude']+','+row['longitude']})
-      venues=venueResponse['venues']
-      for venue in venues:
-          existence = vns.find({'id' : venue['id']})
-          if existence.count()== 0 :              
-              vns.insert(venue)
+def get_venues():
+    with open('cmb_grid_centroids.csv', 'rb') as csvfile:
+        locreader =csv.DictReader(csvfile)
+        for row in locreader:
+          print(row)
+          venueResponse = client.venues.search(params={'ll': row['latitude']+','+row['longitude'],'radius': 715,'intent':'browse'})
+          venues=venueResponse['venues']
+          for venue in venues:
+              vns.update({'id': venue['id']},venue,upsert = True)
+
 
 def generate_venue_list():
     venues = vns.find()
@@ -56,10 +57,16 @@ def generate_venue_list():
         except IndexError:
             p_ctgy = ctgy
             print 'top level category'
-       
-        venue_str = '"'+venue['name']+'",'+'%5f'% venue['location']['lat']+','+'%5f'% venue['location']['lng']+',"'+ctgy+'","'+p_ctgy+'",'+str(venue['stats']['checkinsCount'])+','+str(venue['stats']['usersCount'])     
+
+        venue_str = '"'+venue['name']+'",'+'%5f'% venue['location']['lat']+','+'%5f'% venue['location']['lng']+',"'+ctgy+'","'+p_ctgy+'",'+u','.join((str(venue['stats']['checkinsCount']),str(venue['stats']['usersCount']))).encode('utf-8')
         with open('venues.csv','a') as file:
             file.write(venue_str+'\n')
-             
-            
+
+
+#flow
+categoryResponse = client.venues.categories()
+for category in categoryResponse['categories']:
+    setup_category(category,[],ctgys)
+
+get_venues()
 generate_venue_list()
